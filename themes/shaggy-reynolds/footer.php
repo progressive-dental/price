e7af43a62c5c796719cc4eb3e3ec568ab003a5c2
Fabio Fiori <=>
2017-08-11 16:07:15 -0400

    Fifty fifty reverse fix on mobile. footer and btn fixes.

--- themes/shaggy-reynolds/footer.php
+++ themes/shaggy-reynolds/footer.php
@@ -21,10 +21,10 @@ global $progressive; ?>
         <div class="vc_col-sm-12">
           <div class="headline headline--center">
             <h2 class="headline__main  headline__cta  text-light">
-              Schedule Your Next <strong>Appointment</strong>
+              Book Your <strong>Consultation</strong>
             </h2>
-            <a class="btn btn--primary vc_btn3-color-btn--primary" href="/contact-us" title="Schedule Consultation">
-              Schedule Consultation
+            <a class="btn btn--outline-light" href="/contact-us" title="Inquire Now">
+              Inquire Now
             </a>
           </div>
         </div>
@@ -35,55 +35,12 @@ global $progressive; ?>
     <div class="row">
       <div class="col-md-3">
         <div class="site-foot__column">
-          <a href="<?php echo esc_url( home_url() ); ?>" class="site-foot__header"><img src="<?php echo $progressive['footer-logo']['url']; ?>" alt="<?php echo get_bloginfo('name'); ?>"></a>
-          <ul class="contact-list">
-            <?php if( $progressive['enable-current-patient-number'] == 1 ) : ?>
-              <li class="contact-list__item">
-                Current Patient: <a class="site-foot__link  site-foot__link--phone" href="tel:+1-<?php echo localize_us_number( $progressive['current-patient-number'] ); ?>"><?php echo $progressive['current-patient-number']; ?></a>
-              </li>
-            <?php endif; ?>
-            <?php if( $progressive['enable-new-patient-number'] == 1 ) : ?>
-              <li class="contact-list__item">
-                <?php if( $progressive['enable-ppc'] == 1 ) : ?>
-                  New Patient: <a href="+1-<?php echo localize_us_number( $progressive['new-patient-number'] ); ?>" class="site-foot__link  site-foot__link--phone  clickToCall" data-call-tracking-number="<?php echo localize_us_number( $progressive['new-patient-number'] ); ?>" data-ppc-tracking-number="<?php echo $progressive['ppc-number']; ?>"><span class="webPpcNumber"><?php echo $progressive['ppc-number']; ?></span></a>
-                <?php else : ?>
-                  New Patient: <a class="site-foot__link  site-foot__link--phone" href="tel:+1-<?php echo localize_us_number( $progressive['new-patient-number'] ); ?>"><?php echo $progressive['new-patient-number']; ?></a>
-                <?php endif; ?>
-              </li>
-            <?php endif; ?>
-            <?php if( $progressive['enable-practice-address'] == 1 ) : ?>
-              <li class="contact-list__item">
-                <address><?php echo $progressive['address-line-one']; ?><?php echo ( !empty( $progressive['address-line-two'] ) ? ', ' . $progressive['address-line-two'] : '' ); ?><br> <?php echo $progressive['address-city']; ?>, <?php echo $progressive['address-state']; ?> <?php echo $progressive['address-zip']; ?></address>
-              </li>
-            <?php endif; ?>
-            <?php if( $progressive['enable-youtube'] == 1 || $progressive['enable-google-plus'] == 1 || $progressive['enable-twitter'] == 1 ||
-            $progressive['enable-instagram'] == 1 || $progressive['enable-facebook'] == 1 ) : ?>
-            <li class="contact-list__item">
-              <ul class="social-list">
-                <?php if( $progressive['enable-facebook'] == 1 ) : ?>
-                  <li class="social-list__item"><a href="<?php echo $progressive['facebook-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--facebook"></i></a></li>
-                <?php endif; ?>
-                <?php if( $progressive['enable-google-plus'] == 1 ) : ?>
-                  <li class="social-list__item"><a href="<?php echo $progressive['google-plus-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--google-plus"></i></a></li>
-                <?php endif; ?>
-                <?php if( $progressive['enable-twitter'] == 1 ) : ?>
-                  <li class="social-list__item"><a href="<?php echo $progressive['twitter-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--twitter"></i></a></li>
-                <?php endif; ?>
-                <?php if( $progressive['enable-youtube'] == 1 ) : ?>
-                  <li class="social-list__item"><a href="<?php echo $progressive['youtube-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--youtube"></i></a></li>
-                <?php endif; ?>
-                <?php if( $progressive['enable-instagram'] == 1 ) : ?>
-                  <li class="social-list__item"><a href="<?php echo $progressive['instagram-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--instagram"></i></a></li>
-                <?php endif; ?>
-              </ul>
-            </li>
-          <?php endif; ?>
-        </ul>
+          <a href="<?php echo esc_url( home_url() ); ?>" class="site-foot__header site-foot__header--logo"><img src="<?php echo $progressive['footer-logo']['url']; ?>" alt="<?php echo get_bloginfo('name'); ?>"></a>
       </div>
     </div>
     <div class="col-md-6">
       <div class="site-foot__column  site-foot__column--center">
-        <span class="site-foot__header">Our Dentistry</span>
+        <span class="site-foot__header">Explore Our Site</span>
         <?php
         wp_nav_menu(
           array(
@@ -116,7 +73,50 @@ global $progressive; ?>
       <div class="site-foot__column  site-foot__column--right">
         <?php if( $progressive['enable-location-two'] == 0)  : ?>
           <span class="site-foot__header">Get in Touch</span>
-          <a href="<?php echo get_permalink( $progressive['contact-us-link'] ); ?>" class="btn  btn--primary  btn--small">Contact Us</a>
+          <ul class="contact-list">
+            <?php if( $progressive['enable-current-patient-number'] == 1 ) : ?>
+              <li class="contact-list__item">
+                Current Patient: <a class="site-foot__link  site-foot__link--phone" href="tel:+1-<?php echo localize_us_number( $progressive['current-patient-number'] ); ?>"><?php echo $progressive['current-patient-number']; ?></a>
+              </li>
+            <?php endif; ?>
+            <?php if( $progressive['enable-new-patient-number'] == 1 ) : ?>
+              <li class="contact-list__item">
+                <?php if( $progressive['enable-ppc'] == 1 ) : ?>
+                  For Inquiries: <a href="+1-<?php echo localize_us_number( $progressive['new-patient-number'] ); ?>" class="site-foot__link  site-foot__link--phone  clickToCall" data-call-tracking-number="<?php echo localize_us_number( $progressive['new-patient-number'] ); ?>" data-ppc-tracking-number="<?php echo $progressive['ppc-number']; ?>"><span class="webPpcNumber"><?php echo $progressive['ppc-number']; ?></span></a>
+                <?php else : ?>
+                  For Inquiries: <a class="site-foot__link  site-foot__link--phone" href="tel:+1-<?php echo localize_us_number( $progressive['new-patient-number'] ); ?>"><?php echo $progressive['new-patient-number']; ?></a>
+                <?php endif; ?>
+              </li>
+            <?php endif; ?>
+            <?php if( $progressive['enable-practice-address'] == 1 ) : ?>
+              <li class="contact-list__item">
+                <address><?php echo $progressive['address-line-one']; ?><?php echo ( !empty( $progressive['address-line-two'] ) ? ', ' . $progressive['address-line-two'] : '' ); ?><br> <?php echo $progressive['address-city']; ?>, <?php echo $progressive['address-state']; ?> <?php echo $progressive['address-zip']; ?></address>
+              </li>
+            <?php endif; ?>
+            <?php if( $progressive['enable-youtube'] == 1 || $progressive['enable-google-plus'] == 1 || $progressive['enable-twitter'] == 1 ||
+            $progressive['enable-instagram'] == 1 || $progressive['enable-facebook'] == 1 ) : ?>
+            <li class="contact-list__item">
+              <ul class="social-list">
+                <?php if( $progressive['enable-facebook'] == 1 ) : ?>
+                  <li class="social-list__item"><a href="<?php echo $progressive['facebook-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--facebook"></i></a></li>
+                <?php endif; ?>
+                <?php if( $progressive['enable-google-plus'] == 1 ) : ?>
+                  <li class="social-list__item"><a href="<?php echo $progressive['google-plus-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--google-plus"></i></a></li>
+                <?php endif; ?>
+                <?php if( $progressive['enable-twitter'] == 1 ) : ?>
+                  <li class="social-list__item"><a href="<?php echo $progressive['twitter-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--twitter"></i></a></li>
+                <?php endif; ?>
+                <?php if( $progressive['enable-youtube'] == 1 ) : ?>
+                  <li class="social-list__item"><a href="<?php echo $progressive['youtube-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--youtube"></i></a></li>
+                <?php endif; ?>
+                <?php if( $progressive['enable-instagram'] == 1 ) : ?>
+                  <li class="social-list__item"><a href="<?php echo $progressive['instagram-link']; ?>" class="site-foot__link" target="_blank"><i class="icon  icon--instagram"></i></a></li>
+                <?php endif; ?>
+              </ul>
+            </li>
+          <?php endif; ?>
+        </ul>
+          <a href="<?php echo get_permalink( $progressive['contact-us-link'] ); ?>" class="btn btn--outline-light btn--small">Contact Us</a>
         <?php else : ?>
           <span class="site-foot__header">Locations</span>
           <ul class="contact-list">
@@ -146,7 +146,6 @@ global $progressive; ?>
 </div>
 <?php if( $progressive['enable-google-reviews'] == 1 ) : ?>
   <div class="site-foot__reviews">
-    <div class="section--pattern"></div>
     <a href="<?php echo $progressive['google-reviews-link']; ?>" target="_blank">See Our Reviews on Google</a>
   </div>
 <?php endif; ?>
